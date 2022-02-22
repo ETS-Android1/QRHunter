fd8a4aa47f9a56867e9602cc6c41b1d04059b746
Sukanta Saha <sukanta@ualberta.ca>
2022-02-22 14:23:14 -0700

    initial commit for login/signup ui

--- code/app/src/main/java/com/example/qrhunter/MainActivity.java
+++ code/app/src/main/java/com/example/qrhunter/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.qrhunter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin, btnLoginQR, btnSignup;
+    TextInputLayout txtInputUsernameLogin, txtInputPasswordLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLoginQR = findViewById(R.id.btnLoginQR);
+        btnSignup = findViewById(R.id.btnSignup);
+        txtInputUsernameLogin = findViewById(R.id.txtInputUsernameLogin);
+        txtInputPasswordLogin = findViewById(R.id.txtInputPasswordLogin);
+
+        btnSignup.setOnClickListener(view -> {
+            new Signup_Fragment().show(getSupportFragmentManager(), "SIGNUP_FRAGMENT");
+        });
+
     }
 }
---
+++ code/app/src/main/java/com/example/qrhunter/Signup_Fragment.java
@@ -0,0 +1,10 @@
+package com.example.qrhunter;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.DialogFragment;
+
+public class Signup_Fragment extends DialogFragment {
+
+
+}
