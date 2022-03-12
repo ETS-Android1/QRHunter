d3d55f15bae4671c6ef69fd283cd6fe1b8839f9a
ImtiTimti <45047345+ImtiTimti@users.noreply.github.com>
2022-03-11 21:41:00 -0700

    Backend done and is align with QRCode Class
    I took out the dummy classes and synced it to the main qr code class.

--- code/app/src/main/java/com/example/qrhunter/CustomQRList.java
+++ code/app/src/main/java/com/example/qrhunter/CustomQRList.java
@@ -9,15 +9,18 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.firestore.QuerySnapshot;
+
 import java.util.ArrayList;
 
-public class CustomQRList  extends ArrayAdapter<OtherQRCodes>{
-    private ArrayList<OtherQRCodes> otherCodes;
+public class CustomQRList  extends ArrayAdapter<QRCode>{
+    private ArrayList<QRCode> qrCodes;
     private Context context;
 
-    public CustomQRList(Context context, ArrayList<OtherQRCodes> otherCodes) {
-        super(context, 0, otherCodes);
-        this.otherCodes= otherCodes;
+    public CustomQRList(Context context, ArrayList<QRCode> qrCodes) {
+        super(context, 0, qrCodes);
+        this.qrCodes= qrCodes;
         this.context = context;
     }
 
@@ -28,15 +31,17 @@ public class CustomQRList  extends ArrayAdapter<OtherQRCodes>{
         if(view == null){
             view = LayoutInflater.from(context).inflate(R.layout.content, parent,false);
         }
-        OtherQRCodes codes = otherCodes.get(position);
-        TextView location = view.findViewById(R.id.location_text);
+        QRCode codes = qrCodes.get(position);
+        TextView qrRef = view.findViewById(R.id.location_text);
         TextView scanners = view.findViewById(R.id.scanners_text);
-        TextView date= view.findViewById(R.id.date_text);
+        //TextView date = view.findViewById(R.id.date_text);
         TextView points= view.findViewById(R.id.points_text);
-        location.setText("Location: "+codes.getLocation());
-        scanners.setText("Scanners: "+String.valueOf(codes.getNumScanners()));
-        date.setText("Date: "+String.valueOf( codes.getDate()));
-        points.setText("Points: "+String.valueOf(codes.getPoints()));
+        TextView playerName = view.findViewById(R.id.player_text);
+        qrRef.setText("QR code "+codes.getUniqueHash());
+        scanners.setText("Scanners: "+String.valueOf(codes.getNumScans()));
+        //date.setText("Time Created: "+String.valueOf( codes.getTime()));
+        playerName.setText("Player: "+ String.valueOf(codes.getPlayer()));
+        points.setText("Points: "+String.valueOf(codes.getScore()));
         return view;
     }
 
