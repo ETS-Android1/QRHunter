7e08f03d67c0fab53e732be82cb34c3b363d4604
Sergey <sergeykhlynovskiy@gmail.com>
2022-03-13 21:27:43 -0600

    finished intent testing for userqrinfo activity and profileqrinfo acvtivity

--- code/app/src/androidTest/java/com/example/qrhunter/UserQRInfoTest.java
+++ code/app/src/androidTest/java/com/example/qrhunter/UserQRInfoTest.java
@@ -1,3 +1,5 @@
+// This is intent testing the UserQRInfoActivity
+
 package com.example.qrhunter;
 
 import static androidx.test.espresso.Espresso.onData;
@@ -8,6 +10,7 @@ import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.toPackage;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
@@ -20,6 +23,7 @@ import android.app.Activity;
 
 import android.widget.EditText;
 
+import androidx.test.espresso.intent.Intents;
 import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.platform.app.InstrumentationRegistry;
@@ -47,5 +51,14 @@ public class UserQRInfoTest {
         onView(withId(R.id.addComment)).perform(typeText("This is an example comment"));
         onView(withId(R.id.sendComment)).perform(click());
         onData(anything()).inAdapterView(withId(R.id.commentList)).atPosition(0).check(matches(isDisplayed()));
+    }
+
+    /**
+     * We test if activity is started
+     */
+    @Test
+    public void startTest() {
+        // we see if there is a send button then we know that this is UserQRInfoActivity
+        onView(withId(R.id.sendComment)).check(matches(isDisplayed()));
     }
 }
